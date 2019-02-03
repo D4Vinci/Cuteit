47b2dcdaff26ccce7f0ce58fb261d3fcbb52b2fd
Karim shoair <d4vinci@protonmail.com>
2019-02-04 01:56:06 +0200

    Version 0.2;Making the tool more professional, cleaning the code and usable as module

--- Cuteit.py
+++ Cuteit.py
@@ -1,54 +1,94 @@
-#Author:D4Vinci
+# -*- coding: utf-8 -*-
+# Author:D4Vinci
+# Don't touch my code, it's art :D
+import sys, argparse
+try: # Instead of using sys to detect python version
+	input = raw_input
+except:
+	pass
 
-def ip2long(ip):
-      ip = ip.split("/")[0].split(":")[0]
-      p = ip.split(".")
-      return str( ( ( ( ( int(p[0]) * 256 + int(p[1]) ) * 256 ) + int(p[2]) ) * 256 ) + int(p[3]))
-              #p[0] + "." + str( ( ( ( int( p[1] ) * 256 + int( p[2] ) ) * 256 ) + int( p[3] ) ) * 256 ),
-              #p[0] + "." + p[1] +  str( ( int( p[2] ) *256 ) + int( p[3] ) )
+def my_map(fuck,asses):
+	#Because map behaves differently in python 2 and 3, I decided to write my own fuckin version :3
+	lols = []
+	for ass in asses:
+		lols.append( fuck(ass) ) # LOL
+	return lols
 
-def ip2hex(ip):
-      ip = ip.split("/")[0].split(":")[0]
-      p = ip.split(".")
-      return [str( hex( int(p[0]) ) ) +"."+ str( hex( int(p[1]) ) ) +"."+ str( hex( int(p[2]) ) ) +"."+ str( hex( int(p[3]) ) ),
-              str( hex( int(p[0]) ) ) +"."+ str( hex( int(p[1]) ) ) +"."+ str( hex( int(p[2]) ) ) +"."+ str( int(p[3]) ),
-              str( hex( int(p[0]) ) ) +"."+ str( hex( int(p[1]) ) ) +"."+ str( int(p[2]) ) +"."+ str( int(p[3]) ),
-              str( hex( int(p[0]) ) ) +"."+ str( int(p[1]) ) +"."+ str( int(p[2]) ) +"."+ str( int(p[3]) ),
-              "0x"+"0"*8+str( hex( int(p[0]) ) ).replace("0x","") +"."+ "0x"+"0"*6+str( hex( int(p[1]) ) ).replace("0x","") +"."+ "0x"+"0"*4+str( hex( int(p[2]) ) ).replace("0x","")+"."+ "0x"+"0"*2+str( hex( int(p[3]) ) ).replace("0x",""),
-              str( hex( int( ip2long( ip ) ) ) ).replace( "L" , "" )]
+# As python 3 has no long function anymore, I will do mine
+def ip2long(ip, p=3, long_ip=0): # With simple math I could replace the old shitty function with this loop
+	for part in my_map( int,ip.split(".") ):
+		long_ip += pow(256,p)*part
+		p -=1
+	return [ str(long_ip) ]
 
-def ip2Octal(ip):
-      return '.'.join(format(int(x), '04o') for x in ip.split('.'))
+def ip2octal(ip):
+	  return [ '.'.join([ format(int(x), '04o') for x in ip.split('.') ]) ]
 
-def ip_as_urlencoded(ip):
-      ip = ip.split("/")[0]
-      en=""
-      for i in ip :
-            if i.isdigit() :
-                  en += "%3{}".format(i)
-            elif i == "." :
-                  en += "%2E"
-            elif i == ":" :
-                  en += "%3A"
-      return en
+def ip2hex(ip, length=4):
+	# With length I will decided which parts to convert
+	ip_parts = my_map(int,ip.split("."))
+	return ".".join( my_map( hex,ip_parts[:length] ) + my_map(str,ip_parts[length:]) )
+
+def hex_alike(ip):
+	result,n = [],6
+	for x in range(1,5):
+		result.append( ip2hex(ip,x) )
+
+	result.append( "0x"+"0"*8+ip2hex(ip).split(".")[0][2:] )
+	for p in ip2hex(ip).split(".")[1:]:
+		result[4] = ".".join([ result[4], "0x"+"0"*n+p[2:] ])
+		n -= 2
+	return result + [ip2hex( ip2long(ip)[0] ).replace("L","")]
+
+def ip_urlencoded(ip):
+	return [ ( "%2E".join([ "%3"+i for i in ip.split(".") ]) ).replace(":","%3A") ]
 
 def ip_as_url(ip):
-      return [ "http://howsecureismypassword.net@"+str(ip),
-               "http://google.com@"+str( ip2long( ip ) ),
-               "http://facebook.com@"+str( ip2hex( ip )[-1] ),
-               "http://"+str( ip_as_urlencoded(ip) ),
-               "https://www.google.com@search@"+str( ip_as_urlencoded(ip) ),
-               "http://anywebsite@"+str( ip2Octal(ip) )]
+	urls = ["http://","http://howsecureismypassword.net@","http://google.com@accounts@","https://www.facebook.com+settings&tab=privacy@"]
+	return [ u+ip for u in urls]
 
-print "\n Cuteit - Make a malicious ip a bit cuter :D"
-print " Note:don't type a long url because it's converts the ip only.!"
-ip = raw_input("  ip > ")
-ip=ip.replace("http://","")
-print "\n"
-for n,i in enumerate( ip2hex(ip) + ip_as_url(ip) ):
-      if "http" not in i:
-            print " ["+str(n)+"] "+"http://"+i
-      else:
-            print " ["+str(n)+"] "+i
-print " [12] http://" + ip2Octal(ip)
-print " [13] http://" + ip2long(ip)
+def main():
+	parser = argparse.ArgumentParser(prog='Cuteit.py')
+	parser.add_argument("ip", help="IP you want to convert")
+	parser.add_argument("--disable-coloring", action="store_true", help="Disable colored printing")
+	args = parser.parse_args()
+	G,B,R,W,M,C,end = '\033[92m','\033[94m','\033[91m','\x1b[37m','\x1b[35m','\x1b[36m','\033[0m'
+	Bold,underline  = "\033[1m","\033[4m"
+	if args.disable_coloring:
+		G = B = R = W = M = C = Bold = underline = ''
+	print(end+G+Bold+"Cuteit IP obfuscator made with ❤️  By Karim 'D4Vinci' Shoair")
+	ip = args.ip
+	if ip.count(".")!=3:
+		print(end+R+Bold+"Sorry, we convert ips only not urls!"+end)
+		exit(0)
+
+	# My own shit regex :D
+	for shit in ["http://", "https://", "\\", "/"]:
+		while shit in ip:
+			ip = ip.replace(shit,"")
+	ip = ip.split(":")[0]
+
+	formats = {"IP to Long":ip2long, "IP to HEX":hex_alike, "IP to Octal":ip2octal, "IP to urlencoded IP":ip_urlencoded}
+
+	for form in formats:
+		print(end+M+Bold+"- Converting "+form+end)
+		for n,thing in enumerate( formats[form](ip) ):
+			print(end+G+"\t* Using "+end+R+thing+end+G+" form")
+			for i,shape in enumerate(ip_as_url(thing)):
+				print(end+W+"\t\t["+str(i)+"] "+end+G+shape)
+				sys.stdout.flush() # So it prints line by line not hanging
+			print("")
+			sys.stdout.flush()
+
+class lib:
+	def __init__(self, ip):
+		self.ip = ip
+		self.hex        = ip2hex(self.ip)
+		self.long       = ip2long(self.ip)[0]
+		self.oct        = ip2octal(self.ip)[0]
+		self.urlencoded = ip_urlencoded(self.ip)[0]
+		self.hex_parts  = hex_alike(self.ip)[:-1]
+		self.in_urls = lambda ip : ip_as_url(ip)
+
+if __name__ == '__main__':
+	main()
--- README.md
+++ README.md
@@ -1,6 +1,38 @@
-# Cuteit - Make a malicious ip a bit cuter :smile:
+# Cuteit [![Python 3.5](https://img.shields.io/badge/Python-3.5-yellow.svg)](http://www.python.org/download/) [![Python 2.7](https://img.shields.io/badge/Python-2.7-yellow.svg)](http://www.python.org/download/) ![Build Status](https://img.shields.io/badge/Version-0.2-red.svg)
+**IP obfuscator made to make a malicious ip a bit cuter :smile:**
+
 A simple python tool to help you to social engineer, bypass whitelisting firewalls, potentially break regex rules for command line logging looking for IP addresses and obfuscate cleartext strings to C2 locations within the payload.
 
 All of that is simply done with obfuscating ip to many forms.
-# Screenshot
-![alt img](https://github.com/D4Vinci/Cuteit/blob/master/Screenshot.PNG)
+
+## Usage
+```
+usage: Cuteit.py [-h] [--disable-coloring] ip
+
+positional arguments:
+  ip                  IP you want to convert
+
+optional arguments:
+  -h, --help          show this help message and exit
+  --disable-coloring  Disable colored printing
+```
+
+## Screenshot
+![alt img](Screenshot1.png)
+![alt img](Screenshot2.png)
+
+## Using it as a module!
+You can use this script as a module in your python scripts as follows:
+```
+import Cuteit
+convert = Cuteit.lib(ip)
+print(convert.hex)
+```
+and the photo bellow shows that in action:
+![alt img](module.png)
+
+## Donation
+Rewriting and improving this tool took me a lot of focus and time.
+So if this tool has been useful for you, feel free to thank me by buying me a coffee :)
+
+[![Coffee](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://buymeacoffee.com/d4vinci)
